71a64c30c2c1ab289b24db152a2fd5c748340d36
Alimantu <alimantu@mail.ru>
2015-06-22 03:06:40 +0300

    Minor changes

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,10 @@
-trainY = read.table("./GaCD/Dataset/train/y_train.txt")
-trainX = read.table("./GaCD/Dataset/train/X_train.txt")
-testX = read.table("./GaCD//Dataset/test/X_test.txt")
-testY = read.table("./GaCD/Dataset/test/y_test.txt")
-trainSub = read.table("./GaCD/Dataset/train/subject_train.txt")
-testSub = read.table("./GaCD/Dataset/test/subject_test.txt")
-features = read.table("./GaCD/Dataset/features.txt")
+trainY = read.table("./Dataset/train/y_train.txt")
+trainX = read.table("./Dataset/train/X_train.txt")
+testX = read.table("./Dataset/test/X_test.txt")
+testY = read.table("./Dataset/test/y_test.txt")
+trainSub = read.table("./Dataset/train/subject_train.txt")
+testSub = read.table("./Dataset/test/subject_test.txt")
+features = read.table("./Dataset/features.txt")
 
 xData = rbind(trainX, testX)
 yData = rbind(trainY, testY)
@@ -15,7 +15,7 @@ featIndex = grep("mean\\(\\)|std\\(\\)", features[,2])
 data = xData[,featIndex]
 names(data) = features[featIndex, 2]
 
-activNames = read.table("./GaCD/Dataset/activity_labels.txt")
+activNames = read.table("./Dataset/activity_labels.txt")
 yData[,1]  = activNames[yData[, 1], 2]
 names(yData) = c("Activity")
 
@@ -23,5 +23,5 @@ TData = cbind(sub, yData, data)
 
 meltData = melt(TData, id = c("Subject", "Activity"), measure.vars = features[featIndex, 2])
 TMData = dcast(meltData, Activity + Subject ~ variable, mean)
-write.table(TData, "./GaCD/TidyData.txt")
-write.table(TMData, "./GaCD/MeltedTidyData.txt")
+write.table(TData, "./TidyData.txt")
+write.table(TMData, "./MeltedTidyData.txt")
